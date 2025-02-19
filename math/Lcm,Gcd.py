06c1ef316f6bd70d2d4c022c0d846b72cebb13a5
ABDULLAH ALJARBO <abdullah31258@gmail.com>
2025-02-19 09:39:07 +0300

    Update Lcm,Gcd.py

--- math/Lcm,Gcd.py
+++ math/Lcm,Gcd.py
@@ -4,13 +4,13 @@ import math
 import functools
 
 #ناخذ مجموعة ارقام من المستخدم لحسابها
-nums=list(map(int,input("enter a numbers").spilt()))
+nums=list(map(int,input("enter a numbers").split()))
 #ملاحظة نفصل بين الارقام المدخلة باستخدام مسافة
 
  # math حساب الناتج مبشارة باستخدام مكتبة
-Lcm=functools.reduce(math.lcm(nums))
-Gcd=functools.reduce(math.gcd(nums))
+Lcm=functools.reduce(math.lcm,nums)
+Gcd=functools.reduce(math.gcd,nums)
 #هو لتوحيد النواتج الى ناتج واحد functools بمكتبة  reduce سبب استخدام عنصر  
 
-print(f"Lcm is: {Lcm})
-print(f"Gcd is: {Gcd})
+print(f"Lcm is: {Lcm}")
+print(f"Gcd is: {Gcd}")
