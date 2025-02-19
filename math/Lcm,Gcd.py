29f828c1739e7270e93fdac649b583fcec0badc1
ABDULLAH ALJARBO <abdullah31258@gmail.com>
2025-02-19 09:24:57 +0300

    Create Lcm,Gcd.py

---
+++ math/Lcm,Gcd.py
@@ -0,0 +1,16 @@
+#حساب القاسم المشترك الاكبر والمضاعف المشترك الاصغر
+
+import math
+import functools
+
+#ناخذ مجموعة ارقام من المستخدم لحسابها
+nums=list(map(int,input("enter a numbers").spilt()))
+#ملاحظة نفصل بين الارقام المدخلة باستخدام مسافة
+
+ # math حساب الناتج مبشارة باستخدام مكتبة
+Lcm=functools.reduce(math.lcm(nums)
+Gcd==functools.reduce(math.gcd(nums)
+#هو لتوحيد النواتج الى ناتج واحد functools بمكتبة  reduce سبب استخدام عنصر  
+
+print(f"Lcm is: {Lcm})
+print(f"Gcd is: {Gcd})
