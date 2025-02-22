5e7030a0a94962ee8d78081e147ab0b0cefbc860
ABDULLAH ALJARBO <abdullah31258@gmail.com>
2025-02-22 18:09:49 +0300

    Update calculate-difference.py

--- Time/calculate-difference.py
+++ Time/calculate-difference.py
@@ -2,19 +2,19 @@
 import datetime
 
 #اخذ المعلومات من المستخدم
-year_user=int(input("enetr an old year"))
-month_user=int(input("enter an old month"))
-day_user=int(input("enter an old day"))
+year_user=int(input("enetr year a"))
+month_user=int(input("enter month a"))
+day_user=int(input("enter day a"))
 
-year_user2=int(input("enetr a new year"))
-month_user2=int(input("enter a new month"))
-day_user2=int(input("enter a new day"))
+year_user2=int(input("enetr year b"))
+month_user2=int(input("enter month b"))
+day_user2=int(input("enter day b"))
 
 
 #حساب الفارق بالايام
-old_date=datetime.date(year_user,month_user,day_user)
-new_date=datetime.date(year_user2,month_user2,day_user2)
-age_days=(new_date-old_date).days
+date_a =datetime.date(year_user,month_user,day_user)
+date_b =datetime.date(year_user2,month_user2,day_user2)
+age_days=(abs(date_a-date_b).days)
 
 #تحويل الايام الى تاريخ
 years=age_days//365
@@ -23,8 +23,7 @@ months=remaining_days//30
 days=remaining_days%30 
 
 #طباعة النتيجة
-print("the difference is:")
-print("\n")
-print(f"years is: {years}")
-print(f"months is: {months}") 
-print(f"days is: {days}")
+if date_a>date_b:
+    print(f"date a is greater by {years} years , {months} months , {days} days!")
+else:
+    print(f"date b is greater by {years} years , {months} months , {days} days!")
